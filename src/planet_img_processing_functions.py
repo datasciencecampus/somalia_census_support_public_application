23aa36be8dcd1391dc9691bd11a81b7fea5a7bdf
Timothy S Green <62939263+TSGreen@users.noreply.github.com>
2022-12-20 16:51:21 +0000

    feat: Get dates from Planet file naming system

--- src/planet_img_processing_functions.py
+++ src/planet_img_processing_functions.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from datetime import datetime
 
 from functions_library import setup_sub_dir, generate_file_list
 
@@ -13,4 +14,31 @@ observation_list = generate_file_list(
     )
 
 file_names = [file_name.stem for file_name in observation_list]
+
+def extract_dates_from_image_filenames(file_name):
+    """
+    Extract date information from Planet imagery file names.
+
+    e.g. file named 'Doolow_W_50MB_20221101' would return
+    datetime.datetime(2022, 11, 1, 0, 0)
+
+    Parameters
+    ----------
+    file_name : str
+        The file name for given Planet image.
+
+    Returns
+    -------
+    datetime.datetime
+        Date data type from string.
+    """
+    numerical_string = file_name.split("_")[-1]
+    date = datetime.strptime(numerical_string, "%Y%m%d")
+    return date
+
+observation_dates = [
+    extract_dates_from_image_filenames(file_name) for file_name in file_names
+    ]
+
+planet_img = gdal.Open(str(path_to_imgs.resolve()))
 
