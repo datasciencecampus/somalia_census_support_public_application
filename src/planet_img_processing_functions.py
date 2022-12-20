e43407598fb8e1e2fa0e158f4e398b05dc4cfdd2
Timothy S Green <62939263+TSGreen@users.noreply.github.com>
2022-12-20 17:28:00 +0000

    feat: Get a planet image and open it
    TODO: Remove from main and for loop

--- src/planet_img_processing_functions.py
+++ src/planet_img_processing_functions.py
@@ -34,13 +34,13 @@ def unzip_image_files(file_to_unzip):
         zip.extractall(setup_sub_dir(file_to_unzip.parent, file_to_unzip.stem))
         print(f"Files extracted {file_to_unzip.parent.joinpath(file_to_unzip.stem)}")
 
+
 def main():
     data_dir = Path.cwd().parent.joinpath("data")
 
     planet_imgs_path = setup_sub_dir(data_dir, "planet_images")
 
     path_to_imgs = planet_imgs_path.joinpath("Doolow")
-    #"pansharpened_clip.tif"
 
     zipped_observation_list = generate_file_list(
             path_to_imgs, "zip", []
@@ -54,9 +54,18 @@ def main():
 
     for observation in zipped_observation_list:
         dir_path = observation.parent.joinpath(observation.stem)
+
         # if directory does not exist, unzip it first
         if not dir_path.is_dir():
             unzip_image_files(observation)
+
+        tiff_img_list = generate_file_list(
+                dir_path.joinpath("files"), "tif", ["pansharpened_clip"]
+                )
+
+        planet_img = gdal.Open(str(tiff_img_list[0].resolve()))
+
+        return(planet_img)
 
 
 
