612c8e8e5772aa9aca989847d1d4a0174336037f
Timothy S Green <62939263+TSGreen@users.noreply.github.com>
2022-12-20 17:18:13 +0000

    feat: Unzip planet image folders if not done before

--- src/planet_img_processing_functions.py
+++ src/planet_img_processing_functions.py
@@ -1,19 +1,9 @@
 from pathlib import Path
+from osgeo import gdal
 from datetime import datetime
+from zipfile import ZipFile
 
 from functions_library import setup_sub_dir, generate_file_list
-
-data_dir = Path.cwd().parent.joinpath("data")
-
-planet_imgs_path = setup_sub_dir(data_dir, "planet_images")
-
-path_to_imgs = planet_imgs_path.joinpath("Doolow")
-
-observation_list = generate_file_list(
-        path_to_imgs, "zip", []
-    )
-
-file_names = [file_name.stem for file_name in observation_list]
 
 def extract_dates_from_image_filenames(file_name):
     """
@@ -36,9 +26,38 @@ def extract_dates_from_image_filenames(file_name):
     date = datetime.strptime(numerical_string, "%Y%m%d")
     return date
 
-observation_dates = [
-    extract_dates_from_image_filenames(file_name) for file_name in file_names
-    ]
 
-planet_img = gdal.Open(str(path_to_imgs.resolve()))
+def unzip_image_files(file_to_unzip):
+    # opening the zip file in READ mode
+    with ZipFile(file_to_unzip, 'r') as zip:
+        # extracting all the files
+        zip.extractall(setup_sub_dir(file_to_unzip.parent, file_to_unzip.stem))
+        print(f"Files extracted {file_to_unzip.parent.joinpath(file_to_unzip.stem)}")
+
+def main():
+    data_dir = Path.cwd().parent.joinpath("data")
+
+    planet_imgs_path = setup_sub_dir(data_dir, "planet_images")
+
+    path_to_imgs = planet_imgs_path.joinpath("Doolow")
+    #"pansharpened_clip.tif"
+
+    zipped_observation_list = generate_file_list(
+            path_to_imgs, "zip", []
+        )
+
+    zipped_files = [file_name.stem for file_name in zipped_observation_list]
+
+    observation_dates = [
+        extract_dates_from_image_filenames(file_name) for file_name in zipped_files
+        ]
+
+    for observation in zipped_observation_list:
+        dir_path = observation.parent.joinpath(observation.stem)
+        # if directory does not exist, unzip it first
+        if not dir_path.is_dir():
+            unzip_image_files(observation)
+
+
+
 
