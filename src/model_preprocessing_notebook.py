4667e31fc050b82a1254b261af2d2a6af1ae27e0
Timothy S Green <62939263+TSGreen@users.noreply.github.com>
2023-01-27 17:49:23 +0000

    refactor: get preprocessing to work with updated training data
    And general tidy up of code and comments

--- src/model_preprocessing_notebook.py
+++ src/model_preprocessing_notebook.py
@@ -40,37 +40,58 @@ from modelling_preprocessing import rasterize_training_data
 # %%
 data_dir = Path.cwd().parent.joinpath("data")
 planet_imgs_path = setup_sub_dir(data_dir, "planet_images")
+training_masks_dir = setup_sub_dir(data_dir, "training_masks")
 priority_area_geojsons_dir = setup_sub_dir(data_dir, "priority_areas_geojson")
-
-# %% [markdown]
-# ### Preprocess Planet raster
-
-# %%
-raster_file_path = data_dir.joinpath("training_tile1.tif")
-
-# %%
-raster_filepath = raster_file_path
-img_array = return_array_from_tiff(raster_filepath)
-img_arr_reordered = change_band_order(img_array)
-normalised_img = clip_and_normalize_raster(img_arr_reordered, 99)
 
 # %% [markdown]
 # ### Load DSC training data
 
 # %%
-training_data = gpd.read_file(data_dir.joinpath("training_data.geojson"))
+older_version_training_data = False
+
+# %%
+if older_version_training_data:
+    training_data = gpd.read_file(data_dir.joinpath("training_data.geojson"))
+    raster_file_path = data_dir.joinpath("training_tile1.tif")
+else:
+    doolow_training_data_dir= data_dir.joinpath("doolow_training_data")
+    training_data = gpd.read_file(doolow_training_data_dir.joinpath("training_data.shp"))
+    raster_file_path = doolow_training_data_dir.joinpath("doolow_training_raster.tif")
+# TODO: remove the older version stuff when successful run through on newer training data succeeds
+
+# %%
+# Extra column not used and full of missing values
+training_data = training_data.drop(columns=['fid'])
+# Drop missing data as otherwise subseuqent rasterisation crashes
+# TODO: check why missing data exists
+training_data = training_data.dropna()
+training_data.Type.value_counts()
 
 # %% [markdown]
-# ## Process training data into raster
+# ### Process training data into raster
 
 # %%
 building_class_list = ["House", "Tent", "Service"]
 
-segmented_training_arr = rasterize_training_data(training_data, raster_file_path, building_class_list, "test3.tif")
+segmented_training_arr = rasterize_training_data(
+    training_data,
+    raster_file_path,
+    building_class_list,
+    training_masks_dir.joinpath(f"{raster_file_path.stem}_mask.tif")
+)
+
+# %% [markdown]
+# ### Preprocess Planet raster
+
+# %%
+img_array = return_array_from_tiff(raster_file_path)
+img_arr_reordered = change_band_order(img_array)
+normalised_img = clip_and_normalize_raster(img_arr_reordered, 99)
 
 
 # %%
 def reorder_array(img_arr, height_index, width_index, bands_index):
+    # Re-order the array into height, width, bands order. 
     arr = np.transpose(img_arr, axes=[height_index, width_index, bands_index])
     return arr
 
@@ -78,6 +99,9 @@ def reorder_array(img_arr, height_index, width_index, bands_index):
 # %%
 # transpose array to (x, y, band) from (band, x, y)
 normalised_img = reorder_array(normalised_img, 1, 2, 0)
+
+# %% [markdown]
+# ## Check training tile and training mask
 
 # %%
 import matplotlib.pyplot as plt
