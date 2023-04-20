4f2c8681a9a163c1afd1db9a5f49e5809c78e989
josh-ons2022 <118263780+josh-ons2022@users.noreply.github.com>
2023-04-20 10:44:41 +0100

    added warning to see if number of imgs match masks

--- src/preingress_notebook.py
+++ src/preingress_notebook.py
@@ -17,6 +17,8 @@
 # # Pre GCP Ingress Notebook
 #
 # > Notebook to be run before any files are transferred to the SharePoint GCP ingress folder
+#
+# > You will need to copy contents of GCP_ingress folder on Sharepoint to your local machine
 
 # %% [markdown]
 # ## Set-up
@@ -25,6 +27,7 @@
 # Load required libraries
 from pathlib import Path # working with file paths
 import geopandas as gpd # working with geospatial files and data
+import warnings # used for sending warnings
 
 # Local imports
 from functions_library import setup_sub_dir
@@ -37,6 +40,25 @@ img_dir = setup_sub_dir(training_data_dir, "img") # Note setup_sub_dir creates t
 mask_dir = setup_sub_dir(training_data_dir, "mask")
 
 # %% [markdown]
+# ### Explore files
+
+# %%
+# Get all the img and mask files present
+img_files = list(img_dir.glob("*.tif"))
+mask_files = list(mask_dir.glob("*.geojson"))
+
+# Check that same number of imgs and mask files present
+if len(img_files) != len(mask_files):
+    warnings.warn(f"Number of image files {len(img_files)} doesn't match number of mask files {len(mask_files)}")
+
+# %%
+img_file_names = [file.name for file in img_files]
+img_file_names
+
+# %%
+# do same for mask files here also
+
+# %% [markdown]
 # ## General file cleaning
 #
 # * change all file names to lower case (see [`Path.rename()`](https://docs.python.org/3/library/pathlib.html#pathlib.Path.rename) and [`str.lower()`](https://www.programiz.com/python-programming/methods/string/lower)
@@ -44,16 +66,25 @@ mask_dir = setup_sub_dir(training_data_dir, "mask")
 # * ensure naming convention upheld? Should be: `training_data_<area>_<tile no>_<initials>_<bgr>.tif`
 
 # %%
-# Get all the img and mask files present
-img_files = list(img_dir.glob("*.tif"))
-mask_files = list(mask_dir.glob("*.geojson"))
+img_files
 
-print("there are", len(img_files), "image files and", len(mask_files), "mask files")
+# %%
+img_files[0].name = img_files[0].name.lower()
+
+# %%
+# chop off file name in path lib
+# convert file path to string
+# combine string with lowercase name
+# convert to path
+# rename the file
 
 # %% [markdown]
 # # Image file cleaning
 #
 # * check banding? Check in with Laurence on this and see: https://github.com/datasciencecampus/somalia_unfpa_census_support/issues/173
+
+# %% [markdown]
+# ### List files in mask folder
 
 # %% [markdown]
 # ## Mask file cleaning
@@ -62,7 +93,3 @@ print("there are", len(img_files), "image files and", len(mask_files), "mask fil
 #    * check there is a type column
 #    * remove fid or id column
 #    * check for na
-
-# %%
-
-# %%
