e3caa3dce122809164fc727e817b220b30b7e178
josh-ons2022 <118263780+josh-ons2022@users.noreply.github.com>
2023-04-21 13:51:17 +0100

    added functions to change files to lower case, check mask file has img file, check img file has mask file
    coded along with Joe

--- src/preingress_notebook.py
+++ src/preingress_notebook.py
@@ -28,6 +28,7 @@
 from pathlib import Path # working with file paths
 import geopandas as gpd # working with geospatial files and data
 import warnings # used for sending warnings
+import re # pattern matching
 
 # Local imports
 from functions_library import setup_sub_dir
@@ -65,41 +66,92 @@ img_file_names
 mask_file_names = [file.name for file in mask_files]
 mask_file_names
 
+
 # %% [markdown]
 # ## General file cleaning
 #
 # * change all file names to lower case (see [`Path.rename()`](https://docs.python.org/3/library/pathlib.html#pathlib.Path.rename) and [`str.lower()`](https://www.programiz.com/python-programming/methods/string/lower)
 # * check there each img file has corresponding mask file and _vice versa_ - both img and mask files should have same name except suffix
-# * ensure naming convention upheld? Should be: `training_data_<area>_<tile no>_<initials>_<bgr>.tif`
-# * check banding? Check in with Laurence on this and see: https://github.com/datasciencecampus/somalia_unfpa_census_support/issues/173
+# * ensure naming convention upheld for tif and geojson? Should be: `training_data_<area>_<tile no>_<initials>_<bgr>.tif`
+# * specific for img files! check banding? Check in with Laurence on this and see: https://github.com/datasciencecampus/somalia_unfpa_census_support/issues/173
 
 # %%
-img_files
+def change_to_lower_case(files):
+    
+    # Makes img and mask file names lower case and stores in list
+    files_lower = [Path(file.parent, file.name.lower()) for file in files]
+    
+    # Loop through both lists to rename original img files name
+    for file_original, file_lower in zip(files, files_lower):
+        file_original.rename(file_lower)
+        
+    return files_lower
+
 
 # %%
-# for loop to go through img names and convert to lower case
-for i in range(len(img_files)):
-    lower_case_img_file_names = img_files[i].name.lower()
-    print(lower_case_img_file_names)
+img_files_lower = change_to_lower_case(img_files)
+mask_files_lower = change_to_lower_case(mask_files)
+
 
 # %%
-mask_files
+def check_mask_file_for_img_file(img_files_lower, mask_files_lower):
+    
+    # get mask file names
+    mask_file_names = [mask_file.name for mask_file in mask_files_lower]
+
+    # examine each img file
+    for img_file in img_files_lower:
+        
+        # initialise mask name variable
+        mask_file_name = None
+        
+        # check if banding present
+        if "bgr" in img_file.name or "rgb" in img_file.name:
+            mask_file_name = img_file.name[:-8] + ".geojson"
+        
+        else:
+            mask_file_name = img_file.name[:-4] + ".geojson"
+            warnings.warn(f"banding pattern isn't present in {img_file.name}")
+        
+        # check if mask file present
+        if not mask_file_name in mask_file_names:
+            warnings.warn(f"The mask file ({mask_file_name}) for img_file ({img_file.name}) doesn't exist")
+    
+
 
 # %%
-# for loop to go through mask names and convert to lower case
-for i in range(len(mask_files)):
-    lower_case_mask_file_names = mask_files[i].name.lower()
-    print(lower_case_mask_file_names)
+check_mask_file_for_img_file(img_files_lower, mask_files_lower)
+
 
 # %%
-# chop off file name in path lib
-# convert file path to string
-# combine string with lowercase name
-# convert to path
-# rename the file
+def check_img_file_for_mask_file(img_files_lower, mask_files_lower):
+    
+    # get img file names
+    img_file_names = [img_file.name for img_file in img_files_lower]
+
+    # examine each mask file
+    for mask_file in mask_files_lower:
+        
+        # build img file name
+        img_file_name = mask_file.name[:-8] + "_bgr.tif"
+        
+        # check if img file present
+        if not img_file_name in img_file_names:
+            warnings.warn(f"The img file ({img_file_name}) for mask_file ({mask_file.name}) doesn't exist")
+
 
 # %%
-str(mask_files[0])
+check_img_file_for_mask_file(img_files_lower, mask_files_lower)
+
+# %%
+for file in img_files_lower + mask_files_lower:
+    
+    if re.match(r"training_data_.+_[0-9]+_*", file.name):
+        print("this is great")
+        # warning for when it doesn't match
+
+# %%
+mask_files_lower[0].name
 
 # %% [markdown]
 # ## Mask file cleaning
@@ -115,20 +167,52 @@ str(mask_files[0])
 # assign dataset names from list
 mask_files
 
-# create empty list
-dataframes_list = []
-
 # append geojsons into the list
-for i in range(len(mask_files)):
-    temp_df = gpd.read_file(str(mask_files[i]))
-    dataframes_list.append(temp_df)
+for mask_file in mask_files_lower:
+    temp_df = gpd.read_file(str(mask_file))
     
-# display geopandas dataframe
-for dataset in dataframes_list:
-    display(dataset)
+    # drop fid and id columns
+    
+    # check for type column - if not send error
+    
+        # check any null values in type - send error
+      
+    # check for na values - send error or warning
+    
+    # write back to geojson
+
+
 
 # %%
-dataframes_list[4]
+temp_df = gpd.read_file(str(mask_files_lower[0]))
+
+# %%
+temp_df
+
+# %% [markdown]
+# ### Start data cleaning
+
+# %%
+
+# %%
+# Drop "fid" column
+for i in :
+    
+    if "fid" in dataframes_list:
+        dataset = dataset.drop(columns=["fid"])
+    else:
+        dataset = dataset.drop(columns=["id"])
+
+# %%
+# If statement with warning to see if "Type" column has missing values
+
+    if dataframes_list["Type"].isnull().values.any():
+        warnings.warn(f"Type has null values")
+    else: 
+        print("No null values")
+
+# %%
+dataframes_list[2].head()
 
 # %%
 training_data_baidoa_1_JO = gpd.read_file(mask_dir.joinpath("training_data_baidoa_1_JO.geojson"))
