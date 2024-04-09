e5bd68ac3a017471cb8def030fd9de8fd73ff0a3
Joshua <joshua.onalaja@ons.gov.uk>
2024-04-09 10:15:21 +0000

    moved `generate_tiles` function to `functions_library`, `create_input_tiles` now looks for empty geoms in geojson extents

--- src/create_input_tiles.py
+++ src/create_input_tiles.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 # %%
-import rasterio as rio
 from rasterio.mask import mask
-from rasterio.windows import Window
-import numpy as np
 from pathlib import Path
 import geopandas as gpd
+import rasterio as rio
+from functions_library import generate_tiles
 
 # %% [markdown]
 # #### Set directories
@@ -41,61 +40,30 @@ polygons_gdf = gpd.read_file(baidoa_holwaday_polygons)
 # this creates clipped rasters of the larger polygons - import these into GCP as well as I want to see if we can just run these
 with rio.open(baidoa_holwadag_img) as src:
     for idx, polygon in polygons_gdf.iterrows():
-        out_image, out_transform = mask(src, [polygon.geometry], crop=True)
+        if not polygon.geometry.is_empty:
+            out_image, out_transform = mask(src, [polygon.geometry], crop=True)
 
-        out_meta = src.meta.copy()
-        out_meta.update(
-            {
-                "driver": "GTiff",
-                "height": out_image.shape[1],
-                "width": out_image.shape[2],
-                "transform": out_transform,
-            }
-        )
+            out_meta = src.meta.copy()
+            out_meta.update(
+                {
+                    "driver": "GTiff",
+                    "height": out_image.shape[1],
+                    "width": out_image.shape[2],
+                    "transform": out_transform,
+                }
+            )
 
-        output_file = tile_dir / f"baidoa_holwadag_polygons_{idx}.tif"
+            output_file = tile_dir / f"baidoa_holwadag_polygons_{idx}.tif"
 
-        with rio.open(output_file, "w", **out_meta) as dst:
-            dst.write(out_image)
+            with rio.open(output_file, "w", **out_meta) as dst:
+                dst.write(out_image)
+
+        else:
+            print(f"Skipping empty geometry for index {idx}")
 
 
 # %% [markdown]
 # ### Create tiles
-
-# %%
-
-
-def generate_tiles(image_path, tile_size=384, output_dir="tiles"):
-    output_dir = Path(output_dir)
-    output_dir.mkdir(parents=True, exist_ok=True)
-
-    with rio.open(image_path) as src:
-        rows = src.height // tile_size
-        cols = src.width // tile_size
-
-        for row in range(rows):
-            for col in range(cols):
-                window = Window(col * tile_size, row * tile_size, tile_size, tile_size)
-                # Read the tile
-                tile = src.read(window=window)
-
-                if np.sum(tile) > 0:
-                    tile_meta = src.meta.copy()
-                    tile_meta.update(
-                        {
-                            "width": tile_size,
-                            "height": tile_size,
-                            "transform": rio.windows.transform(window, src.transform),
-                        }
-                    )
-
-                    tile_filename = (
-                        output_dir
-                        / f"{image_path.stem.replace('polygon', '')}_tile_{row}_{col}.tif"
-                    )
-                    with rio.open(tile_filename, "w", **tile_meta) as dst:
-                        dst.write(tile)
-
 
 # %%
 for tile_path in tile_dir.glob("*.tif"):
